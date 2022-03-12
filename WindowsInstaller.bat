d6b19e5a7747e5c91c947a5202f9132fe6f78697
JB <jonathanmb2000@outlook.com>
2022-03-12 16:05:11 -0600

    Added tex and remarked out some bullshit

--- WindowsInstaller.bat
+++ WindowsInstaller.bat
@@ -1,24 +1,49 @@
+@echo off
+echo Installing 7zip
 winget install 7zip.7zip
+echo Installing Adobe Reader
 winget install Adobe.Acrobat.Reader.32-bit
+echo Installing flux
 winget install flux.flux
-winget install Melvin-Abraham.Google-Assistant
+rem echo Installing Google Assistant
+rem winget install Melvin-Abraham.Google-Assistant
+echo Installing Google Chrome
 winget install Google.Chrome
+echo Installing Google Drive
 winget install Google.Drive
+echo Installing Google Earth Pro
 winget install Google.EarthPro
+echo Installing Greenshot
 winget install Greenshot.Greenshot
+echo Installing HackFonts
 winget install SourceFoundry.HackFonts
+echo Installing KeePass
 winget install DominikReichl.KeePass
-winget install Malwarebytes.Malwarebytes
+rem echo Installing Malwarebytes
+rem winget install Malwarebytes.Malwarebytes
+echo Installingd Microsoft VS Code
 winget install Microsoft.VisualStudioCode
+echo Installing Mozilla Firefox
 winget install Mozilla.Firefox
+echo Installing Mozilla Thunderbird Beta
 winget install Mozilla.Thunderbird.Beta
-winget install clsid2.mpc-hc
+echo Installing PotPlayer
+winget install Daum.PotPlayer
+echo Installing Notepad++
 winget install Notepad++.Notepad++
+echo Installing Private Internet Access
 winget install PrivateInternetAccess.PrivateInternetAccess
-winget install Telerik.Fiddler.Classic
+echo Installing Charles Proxy Debugging Tool
+winget install XK72.Charles
+echo Installing Python 3
 winget install Python.Python.3
+echo Installing QbitTorrent Enhanced Edition
 winget install c0re100.qBittorrent-Enhanced-Edition
+echo Installing Sidequest
 winget install SideQuestVR.SideQuest
+echo Installing Ookla Speedtest
 winget install Ookla.Speedtest
+echo Installing Twitch
 winget install Twitch.Twitch
+echo Installing Microsoft PowerToys
 winget install Microsoft.PowerToys
